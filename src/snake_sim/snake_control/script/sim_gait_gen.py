5b4144896f9b02e084120d4273991c18083ddc0f
bong <doorebong@dgist.ac.k>
2020-09-14 13:39:27 +0900

    snake orientation updated  and gazebo start paused

--- src/snake_sim/snake_control/script/sim_gait_gen.py
+++ src/snake_sim/snake_control/script/sim_gait_gen.py
@@ -27,6 +27,7 @@ pub_com_16 = rospy.Publisher('/snake/16_joint_position_controller/command', data
 
 thetas = []
 count = 0
+delay_sec = rospy.Duration(nsecs=3000)
 phase_ver =  3.1415 / 6
 phase_hor =  3.1415 / 6
 
@@ -36,34 +37,50 @@ for i in range(16):
 def motionCalculate():
     global count
 
-    thetas[1] = math.cos(count + phase_ver * 1)
-    thetas[3] = math.cos(count + phase_ver * 3)
-    thetas[5] = math.cos(count + phase_ver * 5)
-    thetas[7] = math.cos(count + phase_ver * 7)
-    thetas[9] = math.cos(count + phase_ver * 9)
-    thetas[11] = math.cos(count + phase_ver * 11)
-    thetas[13] = math.cos(count + phase_ver * 13)
-    thetas[15] = math.cos(count + phase_ver * 15)
+    thetas[0] = 0.46565384 * math.cos(count + phase_ver * 1)
+    thetas[2] = 0.46565384 * math.cos(count + phase_ver * 3)
+    thetas[4] = 0.46565384 * math.cos(count + phase_ver * 5)
+    thetas[6] = 0.46565384 * math.cos(count + phase_ver * 7)
+    thetas[8] = 0.46565384 * math.cos(count + phase_ver * 9)
+    thetas[10] = 0.46565384 * math.cos(count + phase_ver * 11)
+    thetas[12] = 0.46565384 * math.cos(count + phase_ver * 13)
+    thetas[14] = 0.46565384 * math.cos(count + phase_ver * 15)
      
     count+=1
 
 def commandSend():
     pub_com_1.publish(thetas[0])
+    rospy.sleep(delay_sec)
     pub_com_2.publish(thetas[1])
+    rospy.sleep(delay_sec)
     pub_com_3.publish(thetas[2])
+    rospy.sleep(delay_sec)
     pub_com_4.publish(thetas[3])
+    rospy.sleep(delay_sec)
     pub_com_5.publish(thetas[4])
+    rospy.sleep(delay_sec)
     pub_com_6.publish(thetas[5])
+    rospy.sleep(delay_sec)
     pub_com_7.publish(thetas[6])
+    rospy.sleep(delay_sec)
     pub_com_8.publish(thetas[7])
+    rospy.sleep(delay_sec)
     pub_com_9.publish(thetas[8])
+    rospy.sleep(delay_sec)
     pub_com_10.publish(thetas[9])
+    rospy.sleep(delay_sec)
     pub_com_11.publish(thetas[10])
+    rospy.sleep(delay_sec)
     pub_com_12.publish(thetas[11])
+    rospy.sleep(delay_sec)
     pub_com_13.publish(thetas[12])
+    rospy.sleep(delay_sec)
     pub_com_14.publish(thetas[13])
+    rospy.sleep(delay_sec)
     pub_com_15.publish(thetas[14])
+    rospy.sleep(delay_sec)
     pub_com_16.publish(thetas[15])
+    rospy.sleep(delay_sec)
 
 if __name__ == '__main__':
     rospy.init_node('snake_gait_generator',anonymous=True)
@@ -75,4 +92,7 @@ if __name__ == '__main__':
         commandSend()
 
         rate.sleep()
+        # rospy.sleep(0.5)
+        # rospy.spin()
+        
         
