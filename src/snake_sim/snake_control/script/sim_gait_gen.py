674c2a8051ce7af63f45a1e1fd63fe91bff2ae19
bong <doorebong@dgist.ac.k>
2020-09-22 20:23:31 +0900

    randomness and csv log output is added

--- src/snake_sim/snake_control/script/sim_gait_gen.py
+++ src/snake_sim/snake_control/script/sim_gait_gen.py
@@ -4,13 +4,18 @@ import rospy
 import sys
 import math
 import threading
-
+import csv
+import time
+import random
 
 from gazebo_msgs.srv import SetPhysicsProperties
 from gazebo_msgs.msg import ODEPhysics
+from gazebo_msgs.msg import ModelStates
 
+from rosgraph_msgs.msg import Clock
 
 from geometry_msgs.msg import Vector3
+from std_msgs.msg import Time
 from std_msgs.msg import String
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
@@ -34,55 +39,51 @@ pub_com_14 = rospy.Publisher('/snake/14_joint_position_controller/command', data
 pub_com_15 = rospy.Publisher('/snake/15_joint_position_controller/command', data_class=Float64, queue_size= 1)
 pub_com_16 = rospy.Publisher('/snake/16_joint_position_controller/command', data_class=Float64, queue_size= 1)
 
+ros_secs = 0
+ros_nsecs = 0
+
+gazebo_model_pose_x = 0.0
+gazebo_model_pose_y = 0.0
+gazebo_model_pose_z = 0.0
+
+csv_file = open('sim_result.csv', 'a', encoding='utf-8', newline='')
+csv_file.close()
+sim_data_buffer = []
+
 thetas = []
 count = 0
 os_delay_sec = rospy.Duration(nsecs=50000)
 delay_sec = rospy.Duration(nsecs=3000)
-phase_ver =  3.1415 / 6
-phase_hor =  3.1415 / 6
+phase_ver =  (3.1415 / 180) * 30
+amp_ver = (3.1415 / 180) * 30
 
 gazebo_pause = True
 
 for i in range(16):
     thetas.append(0)
 
-def pauseSimulation():
-    global gazebo_pause
-    if gazebo_pause:
-        rospy.wait_for_service('/gazebo/unpause_physics')
-        try:
-            unpause_gazebo = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
-            gazebo_pause = False
-            unpause_gazebo()
-            print('Gazebo Simulation is Starting Now...')
-        except rospy.ServiceException as exc:
-            print("Service did not process request: " + str(exc))
-    else:
-        rospy.wait_for_service('/gazebo/pause_physics')
-        try:
-            pause_gazebo = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
-            gazebo_pause = True
-            pause_gazebo()
-            print('Gazebo Simulation is Stopping Now...')
-        except rospy.ServiceException as exc:
-            print("Service did not process request: " + str(exc))
+def callback_clock(data):
+    global ros_secs
+    global ros_nsecs
+    ros_secs = data.clock.secs
+    ros_nsecs = data.clock.nsecs
 
-def resetWorld():
-    rospy.wait_for_service('/gazebo/reset_world')
-    try:
-        reset_world = rospy.ServiceProxy('/gazebo/reset_world', Empty)
-        reset_world()
-        print('World is reseted!')
-    except rospy.ServiceException as exc:
-        print("Service did not process request: " + str(exc))
+def callback_states(data):
+    global ros_secs
+    global ros_nsecs
 
-def clearSimulation():
-    pauseSimulation()
-    resetWorld()
-    pauseSimulation()
+    global gazebo_model_pose_x
+    global gazebo_model_pose_y
+    global gazebo_model_pose_z
 
-def setTimer(sec = 5, func = clearSimulation):
-    threading.Timer(sec,func).start()
+    global sim_data_buffer
+
+    gazebo_model_pose_x = data.pose[1].position.x
+    gazebo_model_pose_y = data.pose[1].position.y
+    gazebo_model_pose_z = data.pose[1].position.z
+
+    sim_data_buffer.append([ros_secs,ros_nsecs,gazebo_model_pose_x,gazebo_model_pose_y,gazebo_model_pose_z])
+    # print("%d | %d | %f" %(ros_secs,ros_nsecs,gazebo_model_pose_x),end='\n')
 
 def gazeboPhysicsSet(time_step_value = 0.001, max_update_rate_value = 1000):
 # Set as Default Gazebo Property
@@ -104,16 +105,34 @@ def gazeboPhysicsSet(time_step_value = 0.001, max_update_rate_value = 1000):
 def motionCalculate():
     global count
 
-    thetas[0] = 0.46565384 * math.cos(count + phase_ver * 1)
-    thetas[2] = 0.46565384 * math.cos(count + phase_ver * 3)
-    thetas[4] = 0.46565384 * math.cos(count + phase_ver * 5)
-    thetas[6] = 0.46565384 * math.cos(count + phase_ver * 7)
-    thetas[8] = 0.46565384 * math.cos(count + phase_ver * 9)
-    thetas[10] = 0.46565384 * math.cos(count + phase_ver * 11)
-    thetas[12] = 0.46565384 * math.cos(count + phase_ver * 13)
-    thetas[14] = 0.46565384 * math.cos(count + phase_ver * 15)
+    thetas[0] = amp_ver * math.cos(count + phase_ver * 1)
+    thetas[2] = amp_ver * math.cos(count + phase_ver * 3)
+    thetas[4] = amp_ver * math.cos(count + phase_ver * 5)
+    thetas[6] = amp_ver * math.cos(count + phase_ver * 7)
+    thetas[8] = amp_ver * math.cos(count + phase_ver * 9)
+    thetas[10] = amp_ver * math.cos(count + phase_ver * 11)
+    thetas[12] = amp_ver * math.cos(count + phase_ver * 13)
+    thetas[14] = amp_ver * math.cos(count + phase_ver * 15)
      
     count+=1
+
+def commandZero():
+    pub_com_1.publish(0.0)
+    pub_com_2.publish(0.0)
+    pub_com_3.publish(0.0)
+    pub_com_4.publish(0.0)
+    pub_com_5.publish(0.0)
+    pub_com_6.publish(0.0)
+    pub_com_7.publish(0.0)
+    pub_com_8.publish(0.0)
+    pub_com_9.publish(0.0)
+    pub_com_10.publish(0.0)
+    pub_com_11.publish(0.0)
+    pub_com_12.publish(0.0)
+    pub_com_13.publish(0.0)
+    pub_com_14.publish(0.0)
+    pub_com_15.publish(0.0)
+    pub_com_16.publish(0.0)
 
 def commandSend():
     rospy.sleep(os_delay_sec)
@@ -180,12 +199,83 @@ def commandSend():
     pub_com_16.publish(thetas[15])
     rospy.sleep(delay_sec)
 
+def clearSimulation():
+    global csv_file
+    global sim_data_buffer
+    global amp_ver
+    global phase_ver
+    global os_delay_sec
+
+    csv_file.close()
+    
+    commandZero()
+
+    rospy.sleep(rospy.Duration(3.0))
+
+    pauseSimulation()
+    
+    resetWorld()
+    
+    amp_ver = random.randint(0,90) * (3.1415 /180)
+    phase_ver = random.randint(0,360) * (3.1415 /180)
+    tmp_sec = random.randint(0,100)
+    os_delay_sec = rospy.Duration(nsecs = tmp_sec * 10000)
+
+    csv_file = open('sim_result.csv', 'a', encoding='utf-8', newline='')
+    csv_line_writer = csv.writer(csv_file)
+    csv_line_writer.writerows(sim_data_buffer)
+
+    csv_line_writer.writerow([str(time.strftime('%c', time.localtime(time.time()))),os_delay_sec.to_sec(),amp_ver / (3.1415 /180),phase_ver / (3.1415 /180),"start"])
+
+    pauseSimulation()
+
+def pauseSimulation():
+    global gazebo_pause
+    if gazebo_pause:
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            unpause_gazebo = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+            gazebo_pause = False
+            unpause_gazebo()
+            print('Gazebo Simulation is Starting Now...')
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+    else:
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            pause_gazebo = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+            gazebo_pause = True
+            pause_gazebo()
+            print('Gazebo Simulation is Stopping Now...')
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+
+def resetWorld():
+    rospy.wait_for_service('/gazebo/reset_world')
+    try:
+        reset_world = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+        reset_world()
+        print('World is reseted!')
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
+
+def setTimer(sec = 5, func = clearSimulation):
+    threading.Timer(sec,func).start()
+
+
 if __name__ == '__main__':
+    sub_clock = rospy.Subscriber('/clock',Clock,callback_clock,queue_size=1000)
+    sub_model_states = rospy.Subscriber('/gazebo/model_states',ModelStates,callback_states,queue_size=1000)
+
+    csv_file = open('sim_result.csv', 'a', encoding='utf-8', newline='')
+    csv_line_writer = csv.writer(csv_file)
+    csv_line_writer.writerow([str(time.strftime('%c', time.localtime(time.time()))),os_delay_sec.to_sec(),amp_ver / (3.1415 /180),phase_ver / (3.1415 /180),"start"])
+
     rospy.init_node('snake_gait_generator',anonymous=True)
     rate = rospy.Rate(20)
     pauseSimulation()
 
-    gazeboPhysicsSet(max_update_rate_value=5000)
+    gazeboPhysicsSet(max_update_rate_value=3000)
 
     t_prior = rospy.Time.now()
 
